e4a12abe824c1c0731a9e8b624798ddbb16376fe
Jonathan Lee <jonathanlee@DN51ut2b.SUNet>
2022-10-29 13:30:46 -0700

    second commit

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
 import { StyleSheet, SafeAreaView, Text } from "react-native";
 import { useSpotifyAuth } from "./utils";
 import { Themes } from "./assets/Themes";
+import Authbutton from "./assets/components/authbutton";
+import Tracklist from "./assets/components/tracklist";
+//import SongItem from "./assets/components/song";
 
 export default function App() {
   // Pass in true to useSpotifyAuth to use the album ID (in env.js) instead of top tracks
-  const { token, tracks, getSpotifyAuth } = useSpotifyAuth();
+   const { token, tracks, getSpotifyAuth } = useSpotifyAuth();
 
+  let contentDisplayed = null;
+
+  if (token) {
+    contentDisplayed = <Tracklist tracks = {tracks} />
+  } else {
+    contentDisplayed = <Authbutton getSpotifyAuth= {getSpotifyAuth}/>
+  }
+  
   return (
     <SafeAreaView style={styles.container}>
-      {/* TODO: Your code goes here */}
-      <Text style={{ color: "white" }}>Welcome to Assignment 3 - Spotify!</Text>
+      {contentDisplayed}
     </SafeAreaView>
   );
 }
+  //   return (
+  //     <SafeAreaView>
+  //       <Authbutton getSpotifyAuth = {getSpotifyAuth}/>
+  //     </SafeAreaView>
+  //   )
+  // }
 
 const styles = StyleSheet.create({
   container: {
